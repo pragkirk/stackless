ca287422e501a819246296697d95fb9e971dcd33
Kirk Knoernschild <pragkirk@kirkk.com>
2013-04-20 07:12:54 -0500

    Removed debug statements

--- web/src/com/extensiblejava/hello/web/LoanServlet.java
+++ web/src/com/extensiblejava/hello/web/LoanServlet.java
@@ -24,37 +24,17 @@ public class LoanServlet extends HttpServlet {
 		this.loanFacade = lf;
 	}
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//resp.setContentType("text/html");
-		//resp.getWriter().write("<html><body><p>Hello OSGI World -- Welcome to Jetty embedded in Felix.</p><p>Say <a href=\"./a/hello.html\"/>hello.</a></p><p>Say <a href=\"./a/goodbye.html\"/>goodbye.</a></p></body></html>"); //$NON-NLS-1$
-		/*String cp = req.getContextPath();
-		System.out.println("cp: " + cp);
-		String pi = req.getPathInfo();
-		System.out.println("PI: " + pi);
-		String pt = req.getPathTranslated();
-		System.out.println("pt: " + pt);
-		String qs = req.getQueryString();
-		System.out.println("qs: " + qs);
-		String ri = req.getRequestURI();
-		System.out.println("ri: " + ri);
-		StringBuffer ru = req.getRequestURL();
-		System.out.println("ru: " + ru);*/
 		
 		String principle = req.getParameter("principle");
 		String rate = req.getParameter("rate");
 		String term = req.getParameter("term");
-		/*System.out.println("Principle: " + principle);
-		System.out.println("rate: " + rate);
-		System.out.println("term: " + term);*/
 		
 		resp.setContentType("application/json");
-		//JsonObject json = JSON
 		Gson gson = new Gson();
 		HashMap<String, Object> pmt = new HashMap<String, Object>();
-		//BigDecimal payment = this.loanFacade.getMonthlyPayment(new BigDecimal("15000"), new BigDecimal("12"), 60);
 		BigDecimal payment = this.loanFacade.getMonthlyPayment(new BigDecimal(principle), new BigDecimal(rate), new Integer(term).intValue());
 		
 		pmt.put("payment",payment);
-		//pmt.put("payment","455");
 		resp.getWriter().print(gson.toJson(pmt));
 		
 	}
